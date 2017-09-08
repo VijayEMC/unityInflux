b2b6b930b89828364ff1ed2a90067aa14eb2ad2c
Kumar <Vijay.Kumar4@emc.com>
2017-09-08 12:57:37 -0700

    update main file

--- unity_influx1.py
+++ unity_influx1.py
@@ -10,7 +10,7 @@ sess = newUnity.startSession()
 #print (newUnity._getMsg(newUnity._doGet("/api/instances/metric/14595")))
 
 # GET LIST OF METRICS WITH NAMES, PATH, DESCRIPTION, ETC.
-response = newUnity._getMsg(newUnity._doGet("/api/types/metric/instances?fields=name,path,isRealtimeAvailable,description,unitDisplayString"))
+response = newUnity._getMsg(newUnity._doGet("/api/types/metric/instances?fields=name,path,isRealtimeAvailable,description,unitDisplayString,path"))
                     
 # EXTRACT METRICS FROM RESPONSE AND PLACE IN ARRAY
 entryObj = []
@@ -20,9 +20,11 @@ for i in response['entries']:
     
 # PRINT OUT METRICS THEN ASK USER TO SELECT METRIC
 print("Available Metrics: ")
+#paths = []
 
 for i in entryObj:    
-    print(str(i['id']) + ": " + i['name'] + ": " + i['description'] + ": Real Time Avail? " + str(i['isRealtimeAvailable']))
+    print(str(i['id']) + ": " + i['name'] + ": " + i['description'] + ": " + i['path'] + ": Real Time Avail? " + str(i['isRealtimeAvailable']))
+    #paths.append(i['path'])
 
 print("\n\n\n")
 metricInput = input("Please enter a metric ID: ")
@@ -40,11 +42,11 @@ postBody["interval"] = 5
 
 # SEND POST REQUEST
 respMessage = newUnity._getMsg(newUnity.unityPost("https://10.4.44.50/api/types/metricRealTimeQuery/instances", json.dumps(postBody)))
-#print(respMessage)
+print(respMessage)
 
 # GET QUERY ID
 queryId = respMessage['content']['id']
-#print(queryId)
+print(queryId)
 
 # GET METRIC REPORT
 ourMetric = newUnity._getMsg(newUnity._doGet("/api/types/metricQueryResult/instances?filter=queryId eq " + str(queryId)))
